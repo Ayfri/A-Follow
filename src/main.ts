8b21909361705bb24ae0a30805a8d2e2c40a28d3
Ayfri <pierre.ayfri@gmail.com>
2025-09-22 19:12:43 +0200

    feat(main.ts): Initialize random positions for ellipse and set frame rate.

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
 import './style.css';
 import p5 from 'p5';
 
+let xPosition = 0;
+let yPosition = 0;
+
 const sketch = (p: p5) => {
 	p.setup = async () => {
 		p.createCanvas(p.windowWidth, p.windowHeight);
+		p.frameRate(60);
+		xPosition = p.random(p.width);
+		yPosition = p.random(p.height);
 	};
 
 	p.draw = async () => {
-		p.background(220);
-		p.ellipse(p.mouseX, p.mouseY, 50, 50);
+		p.background(0);
+		p.ellipse(xPosition, yPosition, 50, 50);
 	};
 
 	p.windowResized = () => {
