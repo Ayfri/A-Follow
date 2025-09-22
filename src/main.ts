53de6643cf5b8db5e3792e858c927003fc772703
Ayfri <pierre.ayfri@gmail.com>
2025-09-22 19:30:11 +0200

    feat(main.ts): Integrate A* pathfinding and highlight selected paths in the grid.

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { aStar, type Position } from './path';
 import './style.css';
 import p5 from 'p5';
 
@@ -14,6 +15,11 @@ let grid: GridCell[] = []; // array to hold grid cell positions
 
 let xPosition = 0; // in cells
 let yPosition = 0; // in cells
+let selectedPath: Position[] = [];
+
+
+let mouseXCell = -1;
+let mouseYCell = -1;
 
 const sketch = (p: p5) => {
 	p.setup = async () => {
@@ -46,8 +52,39 @@ const sketch = (p: p5) => {
 		p.fill(255);
 		p.noStroke();
 		p.ellipse(xPosition * gridSize + gridSize / 2, yPosition * gridSize + gridSize / 2, gridSize, gridSize);
-	};
+		// Highlight selected path
+		p.fill(0, 255, 0, 150);
+		for (const pos of selectedPath) {
+			p.rect(pos.x * gridSize, pos.y * gridSize, gridSize, gridSize);
+		}
 
+		// Highlight active cells
+		p.fill(0, 0, 255, 150);
+		for (let i = 0; i < grid.length; i++) {
+			if (grid[i].state === 1) {
+				p.rect(grid[i].x, grid[i].y, gridSize, gridSize);
+			}
+		}
+
+		// Recalculate path every frame
+		const newSelectedPath = aStar(
+			{ x: xPosition, y: yPosition },
+			{ x: mouseXCell, y: mouseYCell },
+			grid,
+			gridWidth,
+			gridHeight
+		);
+
+		if (newSelectedPath === null) {
+			selectedPath = [];
+		} else {
+			selectedPath = newSelectedPath;
+		}
+	};
+	p.mouseMoved = () => {
+		mouseXCell = Math.floor(p.mouseX / gridSize);
+		mouseYCell = Math.floor(p.mouseY / gridSize);
+	};
 	p.windowResized = () => {
 		p.resizeCanvas(p.windowWidth, p.windowHeight);
 
